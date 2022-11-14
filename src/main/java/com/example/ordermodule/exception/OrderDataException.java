a32cc449b52fbf11bf7d38439d2d675120349bb2
varshap-17 <varshap630@gmail.com>
2022-11-14 18:49:09 +0530

    order exception class

---
+++ src/main/java/com/example/ordermodule/exception/OrderDataException.java
@@ -0,0 +1,7 @@
+package com.example.ordermodule.exception;
+
+public class OrderDataException extends RuntimeException{
+    public OrderDataException(String message) {
+        super(message);
+    }
+}
